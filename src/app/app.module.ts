3af58d0aa0247f5432c010baa0fd8286eb7a4b3b
jay2jaypc <justin.jenkins@personetics.com>
2017-05-01 17:02:42 -0400

    FM5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,6 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { routing } from './app.router';
-import { effects, store, instrumentation } from './store';
-import { SharedModule } from './shared/shared.module';
-import { WeatherService } from './weather/weather.service';
 import { HomeComponent } from './myBank/home/home.component';
 
 @NgModule({
@@ -17,16 +13,8 @@ import { HomeComponent } from './myBank/home/home.component';
   ],
   imports: [
     BrowserModule,
-    SharedModule,
     FormsModule,
     HttpModule,
-    store,
-    effects,
-    routing,
-    instrumentation
-  ],
-  providers: [
-    WeatherService
   ],
   bootstrap: [
     AppComponent
