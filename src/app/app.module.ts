232f18edcd08af243d6bd25c567572606603b721
jay2jaypc <justin.jenkins@personetics.com>
2017-05-02 10:31:54 -0400

    FM22

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './myBank/home/home.component';
 import { SavingTypeComponent } from './myBank/save/type/saving-type/saving-type.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'savings', component: SavingTypeComponent}
+];
 
 @NgModule({
   declarations: [
@@ -17,6 +23,7 @@ import { SavingTypeComponent } from './myBank/save/type/saving-type/saving-type.
     BrowserModule,
     FormsModule,
     HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   bootstrap: [
     AppComponent
