40f3015a4755b84e77c2feb18f74568545d725be
jay2jaypc <justin.jenkins@personetics.com>
2017-05-02 09:20:52 -0400

    FM13

---
+++ src/app/myBank/save/type/saving-type/saving-type.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-saving-type',
+  templateUrl: './saving-type.component.html',
+  styleUrls: ['./saving-type.component.css']
+})
+export class SavingTypeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
