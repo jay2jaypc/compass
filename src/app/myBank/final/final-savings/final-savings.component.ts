6e28c61a01a8ee888a364aafa7b3dccb4c6fc90d
jay2jaypc <justin.jenkins@personetics.com>
2017-05-02 11:46:37 -0400

    FM04

--- src/app/myBank/final/final-savings/final-savings.component.ts
+++ src/app/myBank/final/final-savings/final-savings.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-final-savings',
   templateUrl: './final-savings.component.html',
   styleUrls: ['./final-savings.component.css']
 })
-export class FinalSavingsComponent implements OnInit {
+export class FinalSavingsComponent implements  OnInit{
 
-  constructor() { }
+  info: {total: number};
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.info = {
+      total: this.route.snapshot.params['total']
+    };
   }
 
 }
