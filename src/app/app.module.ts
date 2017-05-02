763a341e58e07c198fab3a8953cbf5cba1062a2a
jay2jaypc <justin.jenkins@personetics.com>
2017-05-02 10:04:09 -0400

    FM21

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './myBank/home/home.component';
+import { SavingTypeComponent } from './myBank/save/type/saving-type/saving-type.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    SavingTypeComponent
   ],
   imports: [
     BrowserModule,
