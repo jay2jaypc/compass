fbcdcb925d8c0137457ef43d37938fa49bf0ed80
jay2jaypc <justin.jenkins@personetics.com>
2017-05-03 18:55:07 -0400

    FM0056

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,15 @@ import { SavingTypeComponent } from './myBank/save/type/saving-type/saving-type.
 import { SavingAmountComponent } from './myBank/save/type/saving-amount/saving-amount.component';
 import { FinalSavingsComponent } from './myBank/final/final-savings/final-savings.component';
 import { CashFlowComponent } from './myBank/save/type/cash-flow/cash-flow.component';
+import { PilotComponent } from './myBank/pilot/pilot.component';
 
 const appRoutes: Routes = [
   { path: 'welcome', component: HomeComponent},
   { path: 'savings', component: SavingTypeComponent},
   { path: 'amount', component: SavingAmountComponent},
   { path: 'confirm/:savings', component: FinalSavingsComponent},
-  { path: 'cashflow', component: CashFlowComponent}
+  { path: 'cashflow', component: CashFlowComponent},
+  { path: 'pilot', component: PilotComponent}
 ];
 
 @NgModule({
@@ -27,6 +29,7 @@ const appRoutes: Routes = [
     SavingAmountComponent,
     FinalSavingsComponent,
     CashFlowComponent,
+    PilotComponent,
   ],
   imports: [
     BrowserModule,
